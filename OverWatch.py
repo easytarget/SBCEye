393852e4ca1b69813e92478492693ebb1a117f7a
Owen Carter <gitlab@easytarget.org>
2021-04-06 19:24:49 +0200

    Twin screen with slide

--- OverWatch.py
+++ OverWatch.py
@@ -16,6 +16,8 @@ import busio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 
+# User Settings
+pause = 3  #delay before screen cycles
 
 # Create the I2C interface.
 i2c = busio.I2C(SCL, SDA)
@@ -58,24 +60,23 @@ draw.rectangle((0, 0, width, height), outline=0, fill=0)
 font = ImageFont.truetype('LiberationMono-Regular.ttf', 16)
 degree_sign= u'\N{DEGREE SIGN}'
 
+def slideout(scroll=1):
+    x = 127
+    while x > 0:
+        disp.scroll(scroll,0)
+        disp.fill_rect(0, 0, scroll, height, 0)
+        disp.show()
+        x = x - scroll
+
+
 while True:
+
+    # Screen 1
 
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width-1,height-1), outline=0, fill=0)
 
-    # Shell scripts for system monitoring from here:
-    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d' ' -f1"
-    IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
-    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
-
-    # Write three lines of text.
-
+    # Write three lines of BME280 sensor data.
     draw.text((0, 5), 'Temp : ' + format(bme280.temperature, '.1f') + degree_sign,  font=font, fill=255)
     draw.text((0, 25), 'Humi : ' + format(bme280.relative_humidity, '.0f') + '%', font=font, fill=255)
     draw.text((0, 45), 'Pres : ' + format(bme280.pressure, '.0f') + 'mb',  font=font, fill=255)
@@ -83,4 +84,29 @@ while True:
     # Display image.
     disp.image(image.transpose(Image.ROTATE_180))
     disp.show()
-    time.sleep(1)
+    time.sleep(pause)
+    slideout(4)
+
+    # Screen 2
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width-1,height-1), outline=0, fill=0)
+
+    # Shell commands to grab system data
+    cmd = "cat /sys/class/thermal/thermal_zone0/temp | awk '{printf \"%.1f\", $1/1000}'"
+    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "top -bn1 | grep load | awk '{printf \"%.3f\", $(NF-1)}'"
+    TOP = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "free -m | awk 'NR==2{printf \"%.1f\", $3*100/$2 }'"
+    MEM = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
+    # Write three lines of BME280 sensor data.
+    draw.text((0, 5), 'CPU  : ' + CPU + degree_sign,  font=font, fill=255)
+    draw.text((0, 25), 'Load : ' + TOP, font=font, fill=255)
+    draw.text((0, 45), 'Mem  : ' + MEM + '%',  font=font, fill=255)
+
+    # Display image.
+    disp.image(image.transpose(Image.ROTATE_180))
+    disp.show()
+    time.sleep(pause)
+    slideout(4)
