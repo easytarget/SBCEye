37b5f62bae6627cd3ea1e4b4e69f532bcf920819
Owen Carter <gitlab@easytarget.org>
2021-04-07 02:55:35 +0200

    Lamp Pin Interrupt and control, display tweaks

--- OverWatch.py
+++ OverWatch.py
@@ -1,10 +1,4 @@
-# SPDX-FileCopyrightText: 2017 Tony DiCola for Adafruit Industries
-# SPDX-FileCopyrightText: 2017 James DeVito for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-# This example is for use on (Linux) computers that are using CPython with
-# Adafruit Blinka to support CircuitPython libraries. CircuitPython does
-# not support PIL/pillow (python imaging library)!
+# Animate the SSD1306 display attached to my OctoPrint server
 
 import time
 import subprocess
@@ -12,101 +6,138 @@ import subprocess
 from board import SCL, SDA
 import busio
 
-# I2C Display
+# I2C 128x64 OLED Display
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 
-# User Settings
-pause = 3  #delay before screen cycles
-
-# Create the I2C interface.
-i2c = busio.I2C(SCL, SDA)
-
-# Create the SSD1306 OLED class.
-# The first two parameters are the pixel width and pixel height.  Change these
-# to the right size for your display!
-disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
-
-# BME280
+# BME280 I2C Tepmerature Pressure and Humidity sensor
 import adafruit_bme280
 
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
+# GPIO light control
+import RPi.GPIO as GPIO           # Allows us to call our GPIO pins and names it just GPIO
 
-# The fun starts here:
+# User Settings
+passes = 4       # number of (1 second) refreshes of a screen before moving to next
+slidespeed = 8   # number of rows to scroll on each animation step
+button_PIN = 27  # Lamp button
+lamp_PIN = 7     # Lamp relay
 
-# Clear display.
+# Create the I2C interface object
+i2c = busio.I2C(SCL, SDA)
+
+# Create the I2C SSD1306 OLED object
+# The first two parameters are the pixel width and pixel height.
+disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
+# Immediately blank the display in case it is showing garbage
 disp.fill(0)
 disp.show()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new("1", (width, height))
+# Create the I2C BME280 sensor object
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
 
-# Get drawing object to draw on image.
+# GPIO etup
+GPIO.setmode(GPIO.BCM)           # Set's GPIO pins to BCM GPIO numbering
+GPIO.setup(button_PIN, GPIO.IN)  # Set our button pin to be an input
+GPIO.setup(lamp_PIN, GPIO.OUT)   # Set our lamp pin to be an output
+
+# Image canvas
+margin = 20   # Space between the screens while transitioning
+width  = disp.width
+span   = width*2 + margin
+height = disp.height
+
+# Create image canvas (with mode '1' for 1-bit color)
+image = Image.new("1", (span, height))
+
+# Get drawing object so we can easily draw on canvas.
 draw = ImageDraw.Draw(image)
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-# Load default font.
-# font = ImageFont.load_default()
-
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the
-# same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
+# Nice font.
+# the LiberationMono-Regular font used here is located in:
+# /usr/share/fonts/truetype/liberation/ on Raspian.
+# If you get an error that it is not present, install it with:
+#   sudo apt install fonts-liberation
 font = ImageFont.truetype('LiberationMono-Regular.ttf', 16)
+
+# Used when displaying items
 degree_sign= u'\N{DEGREE SIGN}'
 
-def slideout(scroll=1):
-    x = 127
-    while x > 0:
-        disp.scroll(scroll,0)
-        disp.fill_rect(0, 0, scroll, height, 0)
-        disp.show()
-        x = x - scroll
+# Commands used to gather CPU data
+cpuCmd = "cat /sys/class/thermal/thermal_zone0/temp | awk '{printf \"%.1f\", $1/1000}'"
+topCmd = "top -bn1 | grep load | awk '{printf \"%.3f\", $(NF-2)}'"
+memCmd = "free -m | awk 'NR==2{printf \"%.1f\", $3*100/$2 }'"
 
+# Local functions
 
-while True:
+def clean():
+    # Draw a black filled box to clear the canvas.
+    draw.rectangle((0,0,span-1,height-1), outline=0, fill=0)
 
-    # Screen 1
-
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0,0,width-1,height-1), outline=0, fill=0)
-
-    # Write three lines of BME280 sensor data.
-    draw.text((0, 5), 'Temp : ' + format(bme280.temperature, '.1f') + degree_sign,  font=font, fill=255)
-    draw.text((0, 25), 'Humi : ' + format(bme280.relative_humidity, '.0f') + '%', font=font, fill=255)
-    draw.text((0, 45), 'Pres : ' + format(bme280.pressure, '.0f') + 'mb',  font=font, fill=255)
-
-    # Display image.
-    disp.image(image.transpose(Image.ROTATE_180))
+def show(xpos=0):
+    # Put a specific area of the canvas onto display 
+    disp.image(image.transform((width,height),Image.EXTENT,(xpos,0,xpos+width,height)).transpose(Image.ROTATE_180))
     disp.show()
-    time.sleep(pause)
-    slideout(4)
+
+def slideout(step=slidespeed):
+    # Slide the display view across the canvas to animate between screens
+    x = 0
+    while x < width + margin:
+        show(x)
+        x = x + step
+    show(width + margin)
+
+def bmeScreen(xpos=0):
+    # Gather BME280 sensor data.
+    TMP = bme280.temperature
+    HUM = bme280.relative_humidity
+    PRE = bme280.pressure
+    # Write Data
+    draw.text((xpos,  5), 'Temp : ' + format(TMP, '.1f') + degree_sign,  font=font, fill=255)
+    draw.text((xpos, 25), 'Humi : ' + format(HUM, '.0f') + '%', font=font, fill=255)
+    draw.text((xpos, 45), 'Pres : ' + format(PRE, '.0f') + 'mb',  font=font, fill=255)
+
+def sysScreen(xpos=0):
+    # Shell commands to grab and parse system data
+    CPU = subprocess.check_output(cpuCmd, shell=True).decode("utf-8")
+    TOP = subprocess.check_output(topCmd, shell=True).decode("utf-8")
+    MEM = subprocess.check_output(memCmd, shell=True).decode("utf-8")
+    # Write data.
+    draw.text((xpos, 5), 'CPU  : ' + CPU + degree_sign,  font=font, fill=255)
+    draw.text((xpos, 25), 'Load : ' + TOP, font=font, fill=255)
+    draw.text((xpos, 45), 'Mem  : ' + MEM + '%',  font=font, fill=255)
+
+def lampInterrupt(channel):
+    # Read the lamp and toggle it..
+    if (GPIO.input(lamp_PIN) == True):
+        GPIO.output(lamp_PIN,False)
+    else:
+        GPIO.output(lamp_PIN,True)
+
+# The fun starts here:
+
+# Set up the lamp interrupt
+GPIO.add_event_detect(button_PIN, GPIO.RISING, lampInterrupt, bouncetime = 300)
+
+# Main loop runs forever
+while True:
+    # Screen 1
+    for i in range(passes):
+        clean()
+        bmeScreen()
+        show()
+        time.sleep(1)
+    # Final update and transition to screen 2
+    bmeScreen()
+    sysScreen(width+margin)
+    slideout()
 
     # Screen 2
-
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0,0,width-1,height-1), outline=0, fill=0)
-
-    # Shell commands to grab system data
-    cmd = "cat /sys/class/thermal/thermal_zone0/temp | awk '{printf \"%.1f\", $1/1000}'"
-    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "top -bn1 | grep load | awk '{printf \"%.3f\", $(NF-1)}'"
-    TOP = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "free -m | awk 'NR==2{printf \"%.1f\", $3*100/$2 }'"
-    MEM = subprocess.check_output(cmd, shell=True).decode("utf-8")
-
-    # Write three lines of BME280 sensor data.
-    draw.text((0, 5), 'CPU  : ' + CPU + degree_sign,  font=font, fill=255)
-    draw.text((0, 25), 'Load : ' + TOP, font=font, fill=255)
-    draw.text((0, 45), 'Mem  : ' + MEM + '%',  font=font, fill=255)
-
-    # Display image.
-    disp.image(image.transpose(Image.ROTATE_180))
-    disp.show()
-    time.sleep(pause)
-    slideout(4)
+    for i in range(passes):
+        clean()
+        sysScreen()
+        show()
+        time.sleep(1)
+    # Final update and transition to screen 1
+    sysScreen()
+    bmeScreen(width+margin)
+    slideout()
