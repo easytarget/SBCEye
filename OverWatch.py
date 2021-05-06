30cdc229feee6fa0b62c7a95221aa2bbd2c9e4c2
Owen Carter <gitlab@easytarget.org>
2021-05-06 17:31:51 +0200

    Now with logging

--- OverWatch.py
+++ OverWatch.py
@@ -1,7 +1,9 @@
+#! /usr/bin/python
 # Animate the SSD1306 display attached to my OctoPrint server
 
 import time
 import subprocess
+import logging
 
 from board import SCL, SDA
 import busio
@@ -16,11 +18,21 @@ import adafruit_bme280
 # GPIO light control
 import RPi.GPIO as GPIO           # Allows us to call our GPIO pins and names it just GPIO
 
-# User Settings
+# Exit Handler
+import atexit
+
 passes = 4       # number of (1 second) refreshes of a screen before moving to next
 slidespeed = 8   # number of rows to scroll on each animation step
 button_PIN = 27  # Lamp button
 lamp_PIN = 7     # Lamp relay
+
+# Logging setup
+logInterval = 60
+print("Starting OverWatch")
+logging.basicConfig(filename='overwatch.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
+logging.info('')
+logging.info("Starting OverWatch")
+logTimer = 0
 
 # Create the I2C interface object
 i2c = busio.I2C(SCL, SDA)
@@ -35,10 +47,13 @@ disp.show()
 # Create the I2C BME280 sensor object
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
 
-# GPIO etup
+# GPIO setup
 GPIO.setmode(GPIO.BCM)           # Set's GPIO pins to BCM GPIO numbering
 GPIO.setup(button_PIN, GPIO.IN)  # Set our button pin to be an input
 GPIO.setup(lamp_PIN, GPIO.OUT)   # Set our lamp pin to be an output
+
+# Remember the current state of the lamp pin
+lampState = GPIO.input(lamp_PIN)
 
 # Image canvas
 margin = 20   # Space between the screens while transitioning
@@ -106,17 +121,51 @@ def sysScreen(xpos=0):
     draw.text((xpos, 25), 'Load : ' + TOP, font=font, fill=255)
     draw.text((xpos, 45), 'Mem  : ' + MEM + '%',  font=font, fill=255)
 
-def lampInterrupt(channel):
+def buttonInterrupt(channel):
     # Read the lamp and toggle it..
+    logging.info('Button pressed')
     if (GPIO.input(lamp_PIN) == True):
         GPIO.output(lamp_PIN,False)
     else:
         GPIO.output(lamp_PIN,True)
 
+def logger():
+    global lampState
+    global logTimer
+    # Check if the lamp has changed state, and log if so
+    if (GPIO.input(lamp_PIN) != lampState):
+        if (GPIO.input(lamp_PIN) == True):
+            logging.info('Lamp ON')
+            lampState = True
+        else:
+            logging.info('Lamp OFF')
+            lampState = False
+    # Now check if logtimer exceeded, and log parameters if so
+    if (time.time() > logTimer+logInterval):
+        logDetails()
+        logTimer = time.time()
+
+def logDetails():
+    TMP = bme280.temperature
+    HUM = bme280.relative_humidity
+    PRE = bme280.pressure
+    CPU = subprocess.check_output(cpuCmd, shell=True).decode("utf-8")
+    TOP = subprocess.check_output(topCmd, shell=True).decode("utf-8")
+    MEM = subprocess.check_output(memCmd, shell=True).decode("utf-8")
+    logging.info('Temp: ' + format(TMP, '.1f') + degree_sign + ', Humi: ' + format(HUM, '.0f') + '%, Pres: ' + format(PRE, '.0f') + 'mb, CPU: ' + CPU + degree_sign + ', Load: ' + TOP + ', Mem: ' + MEM + '%')
+
+def goodBye():
+    logging.info('Exiting')
+
+# User Settings
+
 # The fun starts here:
 
 # Set up the lamp interrupt
-GPIO.add_event_detect(button_PIN, GPIO.RISING, lampInterrupt, bouncetime = 300)
+GPIO.add_event_detect(button_PIN, GPIO.RISING, buttonInterrupt, bouncetime = 400)
+
+logging.info("Init Complete, entering main loop")
+atexit.register(goodBye)
 
 # Main loop runs forever
 while True:
@@ -125,10 +174,12 @@ while True:
         clean()
         bmeScreen()
         show()
+        logger();
         time.sleep(1)
     # Final update and transition to screen 2
     bmeScreen()
     sysScreen(width+margin)
+    logger();
     slideout()
 
     # Screen 2
@@ -136,8 +187,10 @@ while True:
         clean()
         sysScreen()
         show()
+        logger();
         time.sleep(1)
     # Final update and transition to screen 1
     sysScreen()
     bmeScreen(width+margin)
+    logger();
     slideout()
