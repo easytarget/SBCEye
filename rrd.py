288ffe1fdf780a7a96d4b12fa1c5e80ab5448e8e
Owen Carter <gitlab@easytarget.org>
2021-10-20 11:48:51 +0200

    rrd graph arguments

--- rrd.py
+++ rrd.py
@@ -141,22 +141,27 @@ class Robin:
         start = 'end-' + period
         temp_file = tempfile.NamedTemporaryFile(mode='rb', dir='/tmp', prefix='overwatch_graph')
         timestamp = datetime.datetime.now().strftime("%H:%M:%S, %A, %d %B, %Y")
+        rrd_args = ["--full-size-mode",
+                    "--start", start,
+                    "--end", "now",
+                    "--watermark", self.server_name + " :: " + timestamp,
+                    "--width", str(self.wide)]
+        if graph[:4] == 'pin-':
+            rrd_args.extend(["--height", str(self.high/2)])
+        else:
+            rrd_args.extend(["--height", str(self.high)])
+        rrd_args.extend(["DEF:data=" + str(self.db) + ":" + graph.lower() + ":AVERAGE",
+                         *self.style])
+
         if graph == "env-temp":
             try:
                 rrdtool.graph(
                         temp_file.name,
                         "--title", "Environment Temperature: last " + period,
-                        "--width", str(self.wide),
-                        "--height", str(self.high),
-                        "--full-size-mode",
-                        "--start", start,
-                        "--end", "now",
                         "--upper-limit", "50",
                         "--lower-limit", "10",
                         "--left-axis-format", "%3.1lf\u00B0C",
-                        "--watermark", self.server_name + " :: " + timestamp,
-                        "DEF:data=" + str(self.db) + ":" + graph.lower() + ":AVERAGE",
-                        *self.style)
+                        *rrd_args)
             except Exception as rrd_error:
                 print(rrd_error)
         elif graph == "env-humi":
@@ -164,17 +169,10 @@ class Robin:
                 rrdtool.graph(
                         temp_file.name,
                         "--title", "Environment Humidity: last " + period,
-                        "--width", str(self.wide),
-                        "--height", str(self.high),
-                        "--full-size-mode",
-                        "--start", start,
-                        "--end", "now",
                         "--upper-limit", "100",
                         "--lower-limit", "0",
                         "--left-axis-format", "%3.0lf%%",
-                        "--watermark", self.server_name + " :: " + timestamp,
-                        "DEF:data=" + str(self.db) + ":" + graph.lower() + ":AVERAGE",
-                        *self.style)
+                        *rrd_args)
             except Exception as rrd_error:
                 print(rrd_error)
         elif graph == "env-pres":
@@ -182,18 +180,11 @@ class Robin:
                 rrdtool.graph(
                         temp_file.name,
                         "--title", "Environment Pressure: last " + period,
-                        "--width", str(self.wide),
-                        "--height", str(self.high),
-                        "--full-size-mode",
-                        "--start", start,
-                        "--end", "now",
                         "--upper-limit", "1040",
                         "--lower-limit", "970",
                         "--units-exponent", "0",
                         "--left-axis-format", "%4.0lfmb",
-                        "--watermark", self.server_name + " :: " + timestamp,
-                        "DEF:data=" + str(self.db) + ":" + graph.lower() + ":AVERAGE",
-                        *self.style)
+                        *rrd_args)
             except Exception as rrd_error:
                 print(rrd_error)
         elif graph == "sys-temp":
@@ -201,17 +192,10 @@ class Robin:
                 rrdtool.graph(
                         temp_file.name,
                         "--title", "CPU Temperature: last " + period,
-                        "--width", str(self.wide),
-                        "--height", str(self.high),
-                        "--full-size-mode",
-                        "--start", start,
-                        "--end", "now",
                         "--upper-limit", "80",
                         "--lower-limit", "40",
                         "--left-axis-format", "%3.1lf\u00B0C",
-                        "--watermark", self.server_name + " :: " + timestamp,
-                        "DEF:data=" + str(self.db) + ":" + graph.lower() + ":AVERAGE",
-                        *self.style)
+                        *rrd_args)
             except Exception as rrd_error:
                 print(rrd_error)
         elif graph == "sys-load":
@@ -219,18 +203,11 @@ class Robin:
                 rrdtool.graph(
                         temp_file.name,
                         "--title", "CPU Load Average: last " + period,
-                        "--width", str(self.wide),
-                        "--height", str(self.high),
-                        "--full-size-mode",
-                        "--start", start,
-                        "--end", "now",
                         "--upper-limit", "3",
                         "--lower-limit", "0",
                         "--units-exponent", "0",
                         "--left-axis-format", "%2.3lf",
-                        "--watermark", self.server_name + " :: " + timestamp,
-                        "DEF:data=" + str(self.db) + ":" + graph.lower() + ":AVERAGE",
-                        *self.style)
+                        *rrd_args)
             except Exception as rrd_error:
                 print(rrd_error)
         elif graph == "sys-mem":
@@ -238,37 +215,23 @@ class Robin:
                 rrdtool.graph(
                         temp_file.name,
                         "--title", "System Memory Use: last " + period,
-                        "--width", str(self.wide),
-                        "--height", str(self.high),
-                        "--full-size-mode",
-                        "--start", start,
-                        "--end", "now",
                         "--upper-limit", "100",
                         "--lower-limit", "0",
                         "--left-axis-format", "%3.0lf%%",
-                        "--watermark", self.server_name + " :: " + timestamp,
-                        "DEF:data=" + str(self.db) + ":" + graph.lower() + ":AVERAGE",
-                        *self.style)
+                        *rrd_args)
             except Exception as rrd_error:
                 print(rrd_error)
         else:
-            for idx, pin in enumerate(self.pin_map):
+            for _, pin in enumerate(self.pin_map):
                 if graph == "pin-" + pin[0]:
                     try:
                         rrdtool.graph(
                                 temp_file.name,
                                 "--title", pin[0] + " Pin State: last " + period,
-                                "--width", str(self.wide),
-                                "--height", str(self.high/2),
-                                "--full-size-mode",
-                                "--start", start,
-                                "--end", "now",
                                 "--upper-limit", "1.1",
                                 "--lower-limit", "-0.1",
                                 "--left-axis-format", "%3.1lf",
-                                "--watermark", self.server_name + " :: " + timestamp,
-                                "DEF:data=" + str(self.db) + ":" + graph.lower() + ":AVERAGE",
-                                *self.style)
+                                *rrd_args)
                     except Exception as rrd_error:
                         print(rrd_error)
         response = temp_file.read()
