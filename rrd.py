17136cdf632080546cab8b4f5e26837e3ecb1f7a
Owen Carter <gitlab@easytarget.org>
2021-10-21 21:30:37 +0200

    Remove old DB code

--- rrd.py
+++ rrd.py
@@ -45,6 +45,13 @@ class Robin:
                 "RRA:AVERAGE:0.5:1:131040",   # 3 months per minute
                 "RRA:AVERAGE:0.5:60:26352",  # 3 years per hour
                 *ds_list)
+            print(f'''rrdtool.create(
+                str({self.db}),
+                "--start", "now",
+                "--step", "60",
+                "RRA:AVERAGE:0.5:1:131040",   # 3 months per minute
+                "RRA:AVERAGE:0.5:60:26352",  # 3 years per hour
+                {ds_list})''')
         else:
             print("Using existing: " + str(self.db))
 
@@ -65,73 +72,14 @@ class Robin:
                     f"DS:{ds}:GAUGE:60:{mi}:{ma}")
         self.update_template = ds_template[1:]
 
-        print("------------ OLD DB STUFF --------------")
-
-        self.env_db = Path(s.rrd_file_store + "/" + "env.rrd").resolve()
-        self.sys_db = Path(s.rrd_file_store + "/" + "sys.rrd").resolve()
-        self.pin_db = []
-        for pin in range(len(self.pin_map)):
-            self.pin_db.append(Path(s.rrd_file_store + "/" + self.pin_map[pin][0] + ".rrd").resolve())
-
-        # Main RRDtool databases
-        # One DB file with three data sources for the environmental data
-        if self.env:
-            if not self.env_db.is_file():
-                print("Generating " + str(self.env_db))
-                rrdtool.create(
-                    str(self.env_db),
-                    "--start", "now",
-                    "--step", "60",
-                    "RRA:AVERAGE:0.5:1:131040",   # 3 months per minute
-                    "RRA:AVERAGE:0.5:60:26352",   # 3 years per hour
-                    "DS:env-temp:GAUGE:60:U:U",
-                    "DS:env-humi:GAUGE:60:U:U",
-                    "DS:env-pres:GAUGE:60:U:U")
-            else:
-                print("Using existing: " + str(self.env_db))
-
-        # One DB file with three data sources for the system data
-        if not self.sys_db.is_file():
-            print("Generating " + str(self.sys_db))
-            rrdtool.create(
-                str(self.sys_db),
-                "--start", "now",
-                "--step", "60",
-                "RRA:AVERAGE:0.5:1:131040",   # 3 months per minute
-                "RRA:AVERAGE:0.5:60:26352",   # 3 years per hour
-                "DS:sys-temp:GAUGE:60:U:U",
-                "DS:sys-load:GAUGE:60:U:U",
-                "DS:sys-mem:GAUGE:60:U:U")
-        else:
-            print("Using existing: " + str(self.sys_db))
-        # One database file for each GPIO line
-        for pin in range(len(self.pin_map)):
-            if not self.pin_db[pin].is_file():
-                print("Generating " + str(self.pin_db[pin]))
-                rrdtool.create(
-                    str(self.pin_db[pin]),
-                    "--start", "now",
-                    "--step", "60",
-                    "RRA:AVERAGE:0.5:1:131040",   # 3 months per minute
-                    "RRA:AVERAGE:0.5:60:26352",   # 3 years per hour
-                    "DS:status:GAUGE:60:0:1")
-            else:
-                print("Using existing: " + str(self.pin_db[pin]))
-
     def update(self):
         # Update the database with the latest readings
         dataline = "N"
         if len(self.env) > 0:
-            update_cmd = ':' + str(self.env["temperature"]) + ":" + str(self.env["humidity"]) + ":" + str(self.env["pressure"])
-            rrdtool.update(str(self.env_db), "N" + update_cmd)
-            dataline += update_cmd
-        update_cmd = ':' + str(self.sys["temperature"]) + ":" + str(self.sys["load"]) + ":" + str(self.sys["memory"])
-        dataline += update_cmd
-        rrdtool.update(str(self.sys_db), "N" + update_cmd)
+            dataline += ':' + str(self.env["temperature"]) + ":" + str(self.env["humidity"]) + ":" + str(self.env["pressure"])
+        dataline += ':' + str(self.sys["temperature"]) + ":" + str(self.sys["load"]) + ":" + str(self.sys["memory"])
         for idx, pin in enumerate(self.pin):
-            update_cmd = ':' + str(pin)
-            rrdtool.update(str(self.pin_db[idx]), "N" + update_cmd)
-            dataline += update_cmd
+            dataline += ':' + str(pin)
         rrdtool.update(
                 str(self.db),
                 "--template", self.update_template,
