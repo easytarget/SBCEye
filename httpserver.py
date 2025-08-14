f2707f89d94ba40aec5a0ee7e6ade613c28119d8
Owen <owen@easytarget.org>
2025-08-14 13:08:29 +0200

    Show active pins as bold underlined

--- httpserver.py
+++ httpserver.py
@@ -244,8 +244,9 @@ class _BaseRequestHandler(http.server.BaseHTTPRequestHandler):
         if len(http.data.keys() & pinlist.keys()) > 0:
             ret += '<tr><th>GPIO</th></tr>\n'
             for item,name in pinlist.items():
+                em = 'style=" font-weight: bold; text-decoration: underline;"' if http.data[item] == 1 else ''
                 ret += f'<tr><td>{name}:</td><td style="text-align: right;">'\
-                       f'{http.settings.pin_state_names[http.data[item]]}</td></tr>\n'
+                       f'<span {em}>{http.settings.pin_state_names[http.data[item]]}</span></td></tr>\n'
         return ret
 
     def _give_graphlinks(self, skip=""):
