08458e9cbc99614ce287a30a7e77d0116e89a7a7
Owen <owen@easytarget.org>
2025-10-01 14:38:49 +0200

    fluff

--- gpiohandler.py
+++ gpiohandler.py
@@ -68,15 +68,14 @@ class GPIOHandler:
             logging.warning('GPIO pins were specified but pin setup failed, see syslog')
             logging.info('Pin monitoring disabled')
             return
-        self.directions = {}
-        self.consumers = {}
+        self.directions = {}  # used to remember directions for change detection
+        self.consumers = {}   # used to remember consumers for change detection
         for pin in self.pins:
             data[f'pin-{pin}'] = self._value_to_data(self.pins[pin].value)
             self.directions[pin] = self.pins[pin].direction
             self.consumers[pin] = self.pins[pin].consumer
             print('Pin \'{}\': {}'.format(pin, repr(self.pins[pin])[10:-1]))
             logging.info('Pin \'{}\': {}'.format(pin, repr(self.pins[pin])[10:-1]))
-        # Now set Output pins up
         print('GPIO monitoring configured and logging enabled')
         logging.info('GPIO monitoring configured and logging enabled')
         self.available = True
