05fd1195f68731d116682cfd43bc64a4b6afc367
Owen <owen@easytarget.org>
2025-10-02 18:11:58 +0200

    simplified pinreader

--- pinreader.py
+++ pinreader.py
@@ -3,17 +3,23 @@
 import gpiod
 from os import getpid
 
-'''
-PinReader class (dict)
-'''
 INPUT  = gpiod.line.Direction.INPUT
 ACTIVE  = gpiod.line.Value.ACTIVE
+
 class PinReader(dict):
     '''
-    internal instance class
+    PinReader class (dict)
+    A class derived from a dict() that can record and update the status of a list of pins
+
     '''
     class _instance:
+        '''
+        class that holds individual pin instance data and exposes an update() method for the pin
+        '''
         def __init__(self, chip, line):
+            ''' Creates an pin instance object
+                Takes the GPIO chip device specifier (path, or identifier with gpiod v2+)
+                and line offset '''
             self._pid = getpid()  # record PID of process that called init()
             if not gpiod.is_gpiochip_device(chip):
                 raise ValueError('\'{}\' is not a valid GPIO device'.format(chip))
@@ -31,16 +37,19 @@ class PinReader(dict):
             self.get()
 
         def __repr__(self):
+            ''' returns a formatted representation of the pin data '''
             consumer = None if self.consumer is None else '\'{}\''.format(self.consumer)
             return '_instance(chip=\'{}\' line={} consumer={} direction=\'{}\' value={})'\
                    .format(self.chip, self.line, consumer, self.direction, self.value)
 
         def __str__(self):
+            ''' returns a formatted string giving direction, consumer and value '''
             consumer = None if self.consumer is None else '\'{}\''.format(self.consumer)
             return 'direction: {}, consumer: {}, value: {}'\
                    .format(self.direction, consumer, self.value)
 
         def _value(self):
+            ''' reads the value of the pin '''
             try:
                 with self._chip.request_lines(consumer='pinreader-{}'.format(self._pid),
                                               config={self.line: None}) as request:
@@ -53,6 +62,8 @@ class PinReader(dict):
                 return None
 
         def get(self):
+            ''' reads and updates the current status of the pin,
+                the value is only read if the pin is unused '''
             line = self._chip.get_line_info(self.line)
             self.direction = 'input' if line.direction == INPUT else 'output'
             if line.used:
@@ -65,100 +76,31 @@ class PinReader(dict):
 
     ''' class init() '''
     def __init__(self, pinlist, tolerant=False):
+        ''' Main class init():
+            Takes a pin list: a dictionary{} of pin labels, each item being a tuple()
+             of gpio chip path, and line offset.
+            The 'tolerant' flag prevents exceptions if an invalid pin is specified '''
         super().__init__({})
+        self.chips = []
         for label in pinlist:
             try:
-                super().__setitem__(label,
-                                    self._instance(pinlist[label][0],
-                                                pinlist[label][1]))
+                super().__setitem__(label, self._instance(*pinlist[label]))
             except ValueError as e:
                 if not tolerant:
                     raise ValueError('failed to set up pin \'{}\': {}'
                                      .format(label, e))
-
-    def __str__(self):
-        ret = ''
-        for line in super().__iter__():
-            pin = super().__getitem__(line)
-            if pin.value is not None:
-                value = 'low' if pin.value == 0 else 'high'
-                ret += '{} = {} ({})\n'.format(line, value, pin.direction)
             else:
-                ret += '{} = - (\'{}\', {})\n'.format(line, pin.consumer, pin.direction)
-        return ret.rstrip('\n')
+                if pinlist[label][0] not in self.chips:
+                    self.chips.append(pinlist[label][0])
 
     def update(self, items=None):
+        ''' Updates all pins (deault), a pin, or a list of pins '''
         items = [items] if type(items) == str else items
         items = super().keys() if items is None else items
         for line in items:
             super().__getitem__(line).get()
 
-'''
-    HELPER
-'''
-
-def find_pins(device, regex, prefix='AUTO', verbose=False):
-    if not gpiod.is_gpiochip_device(device):
-        raise ValueError('\'{}\' is not a valid GPIO device'.format(device))
-    num_lines = gpiod.Chip(device).get_info().num_lines
-    pinlist = {}
-    with gpiod.Chip(device) as chip:
-        if verbose:
-            print('Searching for pins on \'{}\' that match regex: {}'
-                  .format(chip.path, regex))
-        for line in range(0, num_lines):
-            name = chip.get_line_info(line).name
-            name = '{}{}'.format(prefix, line) if name is None else name
-            if search(regex, name):
-                pinlist[name] = (chip.path, line)
-                if verbose:
-                    print(' adding: \'{}\' (line {})'.format(name, line))
-            elif verbose:
-                    print(' skipping: \'{}\' (line {})'.format(name, line))
-    return pinlist
-
 if __name__ == "__main__":
-    '''
-        DEMO
-    '''
-
-    from sys import argv
-    from time import asctime, sleep
-    from argparse import ArgumentParser
-
-    self = argv[0]
-    desc = 'Display GPIO pin states and value (if availabe) for matching pins '\
-           'on the specified gpio chip. Use \'gpioinfo\' to see available pins.'
-    elog = 'IMPORTANT: use regex wisely, DO NOT use a generic wildcard such as \'.*\'. '\
-           'Requesting pins that are used by the OS may cause conflicts. eg: reading the value '\
-           'of pins labelled \'SD_*\' can cause Disk I/O errors on Raspberry PI\'s.)'
-    parser = ArgumentParser(prog=self, description=desc, epilog=elog)
-    parser.add_argument("-i", "--interval", default=0, help="Interval between updates in seconds, default: 0 (run once)", type=float)
-    parser.add_argument("-c", "--chip", default="/dev/gpiochip0", help="GPIO chip device path, default: /dev/gpiochip0", type=str)
-    parser.add_argument("-r", "--regex", default="^GPIO[0-9]+$", help="RegEX to select pin names, default: ^GPIO[0-9]+$", type=str)
-    parser.add_argument("-v", "--verbose", action="store_true", help="Show chip and regex processing")
-    args = parser.parse_args()
-
-    # Seach for pins using the device and regex determined above
-    pinlist = find_pins(args.chip, args.regex, verbose=args.verbose)
-    if len(pinlist) == 0:
-        print('No matching pins, exiting')
-        exit()
-
-    # Instantiate the pinreader class on this list
-    pinstates = PinReader(pinlist)
-
-    # little function to collate output
-    def out():
-        return '{}: {}\n{}\n'.format(self, asctime(), pinstates)
-
-    # Show initial output
-    print(out(), end='')
-
-    # Now loop forever showing the values if needed
-    while args.interval != 0:
-        sleep(args.interval)
-        pinstates.update()
-        print("\033[F" * (len(pinstates) + 1), end='')
-        for line in out().split('\n')[:-1]:
-            print('\033[K{}'.format(line))
+    from sys import exit
+    print('PinReader{} class, see inline docs')
+    exit()
