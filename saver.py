e4d1d9ff3975e5ff1e1c19158fd169506aad7bcc
Owen <owen@easytarget.org>
2025-10-05 14:28:42 +0200

    be clearer about saver mode

--- saver.py
+++ saver.py
@@ -9,34 +9,33 @@ class Saver:
     Turns the display on/off between specified times
 
     modes:
-        'none': Screensaver disabled
+        'disabled': Screensaver disabled
         'blank': Turn screen off
 
     parameters:
         disp: display driver object
         settings: (tuple) consisting of:
-            mode:   (str)  One of 'none', 'blank'
+            mode:   (str)  One of 'disabled', 'blank'
             start:  (int)  Start time, hour, 0-23
             end:    (int)  End time, hour, 0-23
+
+    provides:
+        check():  Checks state and applies saver as needed
+                  called from a schedule loop by main code
     '''
 
     active = False  # Current state
 
     def __init__(self, disp, settings):
-
         self.disp = disp
         (self.mode, start, end) = settings
-        if self.mode != 'none':
-            logging.info(f'Saver will {self.mode} display between: '\
-                    f'{start:02d}:00 and {end:02d}:00')
-            print(f'Saver will {self.mode} display between: '\
-                    f'{start:02d}:00 and {end:02d}:00',flush=True)
+        if self.mode != 'disabled':
             if (start == end)\
-                    or start not in range(0,23)\
-                    or end not in range(0,23):
-                logging.warning('start/end times identical or out of range; disabling saver')
-                print('start/end times identical or out of range; disabling saver',flush=True)
-                self.mode = 'off'
+                or start not in range(0,23)\
+                or end not in range(0,23):
+                logging.warning('Saver start/end times identical or out of range; disabling')
+                print('Saver start/end times identical or out of range; disabling',flush=True)
+                self.mode = 'disabled'
             elif start < end:
                 self.saver_map = [False]*24
                 for i in range(start, end):
@@ -45,6 +44,10 @@ class Saver:
                 self.saver_map = [True]*24
                 for i in range(end, start):
                     self.saver_map[i] = False
+            logging.info(f'Saver will {self.mode} display between: '\
+                    f'{start:02d}:00 and {end:02d}:00')
+            print(f'Saver will {self.mode} display between: '\
+                    f'{start:02d}:00 and {end:02d}:00',flush=True)
             self.check()
 
 
@@ -62,7 +65,7 @@ class Saver:
     def check(self):
         '''Check the current state vs the time, and apply changes as
         needed.'''
-        if self.mode != 'off':
+        if self.mode != 'disabled':
             hour = time.localtime()[3]
             if self.active != self.saver_map[hour]:
                 self._apply_state(self.saver_map[hour])
